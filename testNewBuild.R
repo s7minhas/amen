706d58cbcfa756ae8522762a9a2a487c07933529
s7minhas <s7minhas@gmail.com>
2016-11-23 12:06:42 -0800

    documentation fix

--- testNewBuild.R
+++ testNewBuild.R
@@ -10,5 +10,6 @@ data(YX_bin_long)
 data(YX_bin_list) # same as above but replicates are stored in list
 
 # original fn
-fitOrig<-ame_rep(YX_bin_long$Y,YX_bin_long$X,burn=5,nscan=5,odens=1,model="bin")
-fitList<-ame_repL(YX_bin_list$Y,YX_bin_list$X,burn=5,nscan=5,odens=1,model="bin")
+fitOrig<-ame_rep(YX_bin_long$Y,YX_bin_long$X,burn=5,nscan=5,odens=1,model="bin",plot=FALSE)
+fitList<-ame_repL(YX_bin_list$Y,YX_bin_list$X,burn=5,nscan=5,odens=1,model="bin",plot=FALSE)
+
